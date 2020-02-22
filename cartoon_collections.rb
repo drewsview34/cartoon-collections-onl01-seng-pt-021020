f615d68ba173c2f155c70c3b7f680589069c473e
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-22 10:01:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |string|
-    cheese_types.include
+    cheese_types.include?(string)
 end
