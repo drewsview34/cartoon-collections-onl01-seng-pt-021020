24e1fe527b90d63fc912ee99fa980de9928314df
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-22 09:48:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  array.each_with_index {|element, i| puts "#{i}"}
+  array.each_with_index {|element, i| puts "#{i + 1}. #{}"}
 end
 
 def summon_captain_planet# code an argument here
